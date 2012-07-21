d4490b85abd5a794ef3997b60669023c1ecbfed7
Steve Jackson <steve.jackson@leandogsoftware.com>
2012-07-21 09:13:27 -0400

    Initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinancesOnRails::Application.config.secret_token = '6b7a3d31e0b69a7c0cc66323335988d052f2c711d816712aafdf3808ccd5abe4821ac433815744a4dfc6c5ef794d155e43fdf3e87323c625795104c57119c277'
